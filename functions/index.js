1bb5eba15e75cf2b58ed6b2620919e0ff5aab4b0
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-20 11:47:09 -0500

    importantdo requerimientos

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
+'use strict';
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const gcs = require('@google-cloud/storage')();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
